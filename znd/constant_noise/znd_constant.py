d008358bdec89d18a13ea9c24899e9e909c986f7
LongJin <61048219+LongJin-lab@users.noreply.github.com>
2023-07-20 07:55:56 +0800

    Update znd_constant.py

--- znd/constant_noise/znd_constant.py
+++ znd/constant_noise/znd_constant.py
@@ -3,60 +3,25 @@ from torch.optim.optimizer import Optimizer, required
 import numpy as np
 
 
+I = 0.3
+I2 = 1
 class ZNDConstant(Optimizer):
-    r"""Implements stochastic gradient descent (optionally with momentum).
-    Nesterov momentum is based on the formula from
-    `On the importance of initialization and momentum in deep learning`__.
-    Args:
-        params (iterable): iterable of parameters to optimize or dicts defining
-            parameter groups去
-        lr (float): learning rate
-        momentum (float, optional): momentum factor (default: 0)
-        weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
-        dampening (float, optional): dampening for momentum (default: 0)
-        nesterov (bool, optional): enables Nesterov momentum (default: False)
-    Example:
-        >>> optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9)
-        >>> optimizer.zero_grad()
-        >>> loss_fn(model(input), target).backward()
-        >>> optimizer.step()
-    __ http://www.cs.toronto.edu/%7Ehinton/absps/momentum.pdf
-    .. note::
-        The implementation of SGD with Momentum/Nesterov subtly differs from
-        Sutskever et. al. and implementations in some other frameworks.
-        Considering the specific case of Momentum, the update can be written as
-        .. math::
-                  v = \rho * v + g \\
-                  p = p - lr * v
-        where p, g, v and :math:`\rho` denote the parameters, gradient,
-        velocity, and momentum respectively.
-        This is in contrast to Sutskever et. al. and
-        other frameworks which employ an update of the form
-        .. math::
-             v = \rho * v + lr * g \\
-             p = p - v
-        The Nesterov version is analogously modified.
-    """
-
-    def __init__(self, params, lr=required, momentum=0, dampening=0,
-                 weight_decay=0, nesterov=False, I=5., D=10.):
-        defaults = dict(lr=lr, momentum=momentum, dampening=dampening,
-                        weight_decay=weight_decay, nesterov=nesterov, I=I, D=D)
+    
+    def __init__(self, params, lr=required, momentum=0, dampening=0, weight_decay=0, nesterov=False):
+        self.I = I
+        self.I2 = I2
+        defaults = dict(lr=lr, momentum=momentum, dampening=dampening,weight_decay=weight_decay,nesterov=nesterov)
         if nesterov and (momentum <= 0 or dampening != 0):
             raise ValueError("Nesterov momentum requires a momentum and zero dampening")
         super(ZNDConstant, self).__init__(params, defaults)
 
     def __setstate__(self, state):
-        super(ZNDConstant, self).__setstate__(state)
+        super(ND, self).__setstate__(state)
         for group in self.param_groups:
             group.setdefault('nesterov', False)
-
-    def step(self, closure=None):
-        """Performs a single optimization step.
-        Arguments:
-            closure (callable, optional): A closure that reevaluates the model
-                and returns the loss.
-        """
+    
+    def step(self, epoch, closure=None):
+        grad_dict = {}  # initial grad_dict
         loss = None
         if closure is not None:
             loss = closure()
@@ -66,33 +31,52 @@ class ZNDConstant(Optimizer):
             momentum = group['momentum']
             dampening = group['dampening']
             nesterov = group['nesterov']
-            I = group['I']
-
             count = 0
             for p in group['params']:
                 if p.grad is None:
                     continue
-
                 d_p = p.grad.data
-                g_buf = d_p
+                grad_dict[epoch] = torch.clone(d_p).detach()  # store current gradient
                 if weight_decay != 0:
                     # gradient = gradient + weight+decay
-                    d_p.add_(weight_decay, p.data)
+                    d_p.add_(p.data, alpha=weight_decay)
                 if momentum != 0:
                     param_state = self.state[p]
                     if 'I_buffer' not in param_state:
-                        I_buf = param_state['I_buffer'] = torch.zeros_like(p.data)
-                        I_buf.mul_(momentum).add_(g_buf)  # I_buf = I_buf * momentum + g_p
+                        I_buf = param_state['I_buffer'] = torch.clone(d_p).detach()  # initial I
                     else:
                         I_buf = param_state['I_buffer']
-                        I_buf.mul_(momentum).add_(1 - dampening, g_buf)  # I_buf = I_buf * momentum + (1 - dampenin) * d_p
+                        prev_grad = grad_dict.get(epoch-1)
+                        if prev_grad is None:
+                            prev_grad = torch.clone(d_p).detach()
+                        I_buf.mul_(momentum).add_(prev_grad, alpha=1 - dampening)
                     if count == 0:
-                        g_buf = d_p
-                        d_p = g_buf.add_(I, I_buf).add_(0.01)  # d_p = d_p + I * I_buf
+                        d_p.add_(0.01)
+                    count += 1      
+                    if epoch < 100:    
+                        d_p.add_(I_buf,alpha = self.I)
                     else:
-                        g_buf = d_p
-                        d_p = g_buf.add_(I, I_buf)
-                    count += 1
-                p.data.add_(-group['lr'], d_p)  # p.data = p.data + lr * (d_p + I * I_buf + D * D_buf)
+                        d_p.add_(I_buf,alpha = self.I2)
+                
+                
+                p.add_(d_p, alpha=-group['lr'])    
+
+                
+                # if momentum != 0:
+                #     param_state = self.state[p]
+                #     if 'grad_buffer' not in param_state:
+                #         g_buf = param_state['grad_buffer'] = torch.zeros_like(p.data)
+                #         g_buf = d_p
+                #         I_buf = param_state['I_buffer'] = torch.zeros_like(p.data)
+                #         I_buf.mul_(momentum).add_(g_buf)  # I_buf = I_buf * momentum + g_buf
+                #     else:
+                #         I_buf = param_state['I_buffer']
+                #         g_buf = param_state['grad_buffer'] # get the previous step gradient
+                #         I_buf.mul_(momentum).add_(g_buf, alpha=1 - dampening)  # I_buf = I_buf * momentum + (1 - dampenin) * g_buf
+                #         self.state[p]["grad_buffer"]=d_p.clone() #store gradient for next iteration
+                   
+                #     d_p = d_p.add_(I_buf, alpha=self.I)
+                    
+                # p.data.add_(d_p, alpha=-group['lr'])  # p.data = p.data + lr * (d_p + I * I_buf + D * D_buf)
 
         return loss
